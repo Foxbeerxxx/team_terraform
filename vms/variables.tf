c96f8a99fb32d81cc41475a4ee9ecfbd26d7bf6f
Alexey Tatarintsev <tatarintsev0810@gmail.com>
2025-06-14 23:37:43 +0300

    4

--- vms/variables.tf
+++ vms/variables.tf
@@ -13,3 +13,24 @@ variable "subnet_id" {
   description = "ID подсети"
   type        = string
 }
+
+
+variable "ip_address" {
+  type        = string
+  description = "ip-адрес"
+  validation {
+    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}$", var.ip_address))
+    error_message = "Значение должно быть валидным IPv4 адресом."
+  }
+}
+
+variable "ip_list" {
+  type        = list(string)
+  description = "список ip-адресов"
+  validation {
+    condition = alltrue([
+      for ip in var.ip_list : can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}$", ip))
+    ])
+    error_message = "Все значения должны быть валидными IPv4 адресами."
+  }
+}
