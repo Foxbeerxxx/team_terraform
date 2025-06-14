3cf47004a0c6edfc6c329781dd3fc90fa209649c
Alexey Tatarintsev <tatarintsev0810@gmail.com>
2025-06-14 21:22:26 +0300

    1 запуск демо , Тейт файл загружен

---
+++ demo/.terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/http" {
+  version = "3.5.0"
+  hashes = [
+    "h1:8bUoPwS4hahOvzCBj6b04ObLVFXCEmEN8T/5eOHmWOM=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.7.2"
+  hashes = [
+    "h1:356j/3XnXEKr9nyicLUufzoF4Yr6hRy481KIxRVpK0c=",
+  ]
+}
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version     = "0.118.0"
+  constraints = "0.118.0"
+  hashes = [
+    "h1:pc0xxK81rkNeoNsog3JRl01q3UQqYCQmyOIa/hrnxnM=",
+  ]
+}
--- vms/providers.tf
+++ vms/providers.tf
@@ -1,11 +1,56 @@
+# коментировал перед s3
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#  required_version = ">= 1.8.4"
+#}
+
 terraform {
+  required_version = "1.8.4"
+
+  backend "s3" {
+    
+    shared_credentials_files = ["~/.aws/credentials"]
+    shared_config_files = [ "~/.aws/config" ]
+    profile = "default"
+    region="ru-central1"
+
+    bucket     = "fbr" #FIO-netology-tfstate
+    key = "production/terraform.tfstate"
+    
+
+    # access_key                  = "..."          #Только для примера! Не хардкодим секретные данные!
+    # secret_key                  = "..."          #Только для примера! Не хардкодим секретные данные!
+
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  endpoints ={
+    #dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gn3ndpua1j6jaabf79/etnij6ph9brodq9ohs8d"
+    s3 = "https://storage.yandexcloud.net"
+  }
+
+    #dynamodb_table              = "tfstate-lock-develop"
+  }
+
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "0.118.0"
     }
   }
-  required_version = ">= 1.8.4"
 }
+
+
+
+
+
 
 provider "yandex" {
   # token                    = "do not use!!!"
